73e6e80b6e4802bae638edc1b861a8c68be224cf
YuliiaLukianiuk <lukianiky@gmail.com>
2021-03-29 22:32:03 +0300

    dispatch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import {addPost, updateNewPostText} from './redux/state';
 
 
@@ -12,16 +12,13 @@ import {addPost, updateNewPostText} from './redux/state';
 ReactDOM.render(
   <React.StrictMode>
     <App state={state} 
-         addPost={addPost} 
-         updateNewPostText={updateNewPostText}/>
+         dispatch={store.dispatch.bind(store)} 
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
 };
 
 
-renderEntireTree(state);
-subscribe(renderEntireTree);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,52 +1,68 @@
-let renderEntireTree = () => {
-  console.log("state  changed");
-};
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likesCount: "25" },
-      { id: 2, message: "It's my first post", likesCount: "2" },
-    ],
-    newPostText: "Yuliia Hello",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: "25" },
+        { id: 2, message: "It's my first post", likesCount: "2" },
+      ],
+      newPostText: "Yuliia Hello",
+    },
+    messagePage: {
+      dialogData: [
+        { id: 1, name: "Yuliia" },
+        { id: 2, name: "Anna" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Victor" },
+        { id: 6, name: "Valera" },
+      ],
+  
+      messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Hello!!!" },
+        { id: 4, message: "Hi" },
+        { id: 5, message: "How are you?" },
+        { id: 6, message: "yo" },
+      ],
+    }
   },
-  messagePage: {
-    dialogData: [
-      { id: 1, name: "Yuliia" },
-      { id: 2, name: "Anna" },
-      { id: 3, name: "Sveta" },
-      { id: 4, name: "Sasha" },
-      { id: 5, name: "Victor" },
-      { id: 6, name: "Valera" },
-    ],
-
-    messagesData: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "Hello!!!" },
-      { id: 4, message: "Hi" },
-      { id: 5, message: "How are you?" },
-      { id: 6, message: "yo" },
-    ],
+  getState() {
+    return this._state
   },
-};
+  _callSubscriber()  {
+    console.log("state  changed");
+  },
+  _addPost()  {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = "";
-  renderEntireTree(state);
-};
+  },
+  _updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
 
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  renderEntireTree(state);
-};
+  },
+  subscribe (observer) {
+    this._callSubscriber = observer;
+  },
+  dispatch(action) {
+    if (action.type==='ADD-POST') {
+      this._addPost();
+    }else if (action.type=== 'UPDATE-NEW-POST-TEXT') {
+      this._updateNewPostText(action.newText);
+    }
+  }
+}
 
-export const subscribe = (observer) => {
-  renderEntireTree = observer;
-};
-export default state;
+
+
+
+export default store;
+window.store = store;
