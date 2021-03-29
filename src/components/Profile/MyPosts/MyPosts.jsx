d5099b0c87abe71e8d56d0ff9aac6f94043e3657
YuliiaLukianiuk <lukianiky@gmail.com>
2021-03-29 23:04:17 +0300

    action creator|type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/state";
 import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
@@ -10,12 +11,13 @@ const MyPosts = (props) => {
     );
 
     let addPost = () => {
-      props.dispatch({ type: 'ADD-POST'});
+      props.dispatch( addPostActionCreator());
     }
 
     let onPostChange = () => {
       let text = newPostElement.current.value;
-        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text})
+        props.dispatch(updateNewPostTextActionCreator(text));
+        
     }
   return (
     <div className={classes.postsBlock}>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 let store = {
   _state: {
     profilePage: {
@@ -16,7 +18,7 @@ let store = {
         { id: 5, name: "Victor" },
         { id: 6, name: "Valera" },
       ],
-  
+
       messagesData: [
         { id: 1, message: "Hi" },
         { id: 2, message: "How are you?" },
@@ -25,15 +27,15 @@ let store = {
         { id: 5, message: "How are you?" },
         { id: 6, message: "yo" },
       ],
-    }
+    },
   },
   getState() {
-    return this._state
+    return this._state;
   },
-  _callSubscriber()  {
+  _callSubscriber() {
     console.log("state  changed");
   },
-  _addPost()  {
+  _addPost() {
     let newPost = {
       id: 5,
       message: this._state.profilePage.newPostText,
@@ -42,27 +44,29 @@ let store = {
     this._state.profilePage.posts.push(newPost);
     this._state.profilePage.newPostText = "";
     this._callSubscriber(this._state);
-
   },
   _updateNewPostText(newText) {
     this._state.profilePage.newPostText = newText;
     this._callSubscriber(this._state);
-
   },
-  subscribe (observer) {
+  subscribe(observer) {
     this._callSubscriber = observer;
   },
   dispatch(action) {
-    if (action.type==='ADD-POST') {
+    if (action.type === "ADD-POST") {
       this._addPost();
-    }else if (action.type=== 'UPDATE-NEW-POST-TEXT') {
+    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
       this._updateNewPostText(action.newText);
     }
-  }
-}
-
-
-
+  },
+};
+export const addPostActionCreator = () => ({
+  type: ADD_POST,
+});
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 
 export default store;
 window.store = store;
