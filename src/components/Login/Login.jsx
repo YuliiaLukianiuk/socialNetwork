86bebeafe927abe4de45b5e48c3eb677fde6befc
YuliiaLukianiuk <lukianiky@gmail.com>
2021-11-23 17:35:03 +0200

    login авторизация

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,42 +1,55 @@
-import React  from "react";
+import React from "react";
 import {Field, reduxForm} from "redux-form";
 import {Input} from "../common/FormsControl";
 import {required} from "../../utils/validate";
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
+import {Redirect} from "react-router-dom";
 
-const LoginForm = (props) =>{
-	return	<form onSubmit={props.handleSubmit}>
+
+const LoginForm = (props) => {
+	return <form onSubmit={props.handleSubmit}>
 		<div>
-			<Field placeholder={'LOGIN'}
-			       name={'login'}
+			<Field placeholder={'Email'}
+			       name={'email'}
 			       component={Input}
-			       validate = {[required]}
+			       validate={[required]}
 			/>
 		</div>
 		<div><Field placeholder={'PASSWORD'}
 		            name={'password'}
 		            component={Input}
-		            validate = {[required]}
+		            type={"password"}
+		            validate={[required]}
 		/></div>
-		<div><Field component={Input} name={'checkbox'} type='checkbox'/>remember me</div>
+		<div><Field component={Input}
+		            name={'rememberMe'}
+		            type='checkbox'/>remember me
+		</div>
 		<div>
 			<button>LOGIN</button>
 		</div>
 	</form>
 }
 const LoginReduxForm = reduxForm({
-	form:'login'
+	form: 'login'
 })(LoginForm)
 
 const Login = (props) => {
+	console.log(props)
 	const onSubmit = (formData) => {
-		console.log(formData)
+		props.login(formData.email, formData.password, formData.rememberMe)
+	}
+	if (props.isAuth) {
+		return <Redirect to = {"/profile"}></Redirect>
 	}
 	return <div>
-	<h1>LOGIN</h1>
-	<LoginReduxForm onSubmit={onSubmit}/>
+		<h1>LOGIN</h1>
+		<LoginReduxForm onSubmit={onSubmit}/>
 	</div>
 }
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth
+})
 
-
-
-export default Login;
+export default connect(mapStateToProps, {login})(Login);
